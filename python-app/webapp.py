0148cd57ebd593375b6a3f4fae96fef953b8ae4b
persiidan <136482383+persiidan@users.noreply.github.com>
2023-09-21 13:20:41 +0300

    Update webapp.py

--- python-app/webapp.py
+++ python-app/webapp.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# MongoDB connection setup
+client = MongoClient('mongodb://root:3yGWpZ7jeS@34.78.116.136:27017/')  # Replace with your MongoDB connection URI
+db = client['persi_project']  # Replace with your database name
+collection = db['links']  # Replace with your collection name
+
+
+@app.template_filter('zip_lists')
+def zip_lists(a, b):
+    return zip(a, b)
+
 @app.route('/')
 def index():
-    # Define your links here
-    linkedin_link = "https://www.linkedin.com/in/idan-persi-81691b288/"
-    gitlab_link = "https://gitlab.com/idanpersi"
-    github_link = "https://github.com/persiidan"
+
+   # Retrieve links from MongoDB
+    links = collection.find_one()['links']
+    
+    # Define labels for the links
+    labels = ['LinkedIn', 'GitHub', 'GitLab']
 
     # Render the HTML template and pass the links as variables
-    return render_template('index.html', linkedin=linkedin_link, gitlab=gitlab_link, github=github_link)
+    return render_template('index.html', links=links, labels=labels)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
-
+     app.run(host="0.0.0.0",port=5000,debug=True)
