c6476a52e48fa0c786421ca68e51f69a3f16d507
persiidan <136482383+persiidan@users.noreply.github.com>
2023-12-20 12:50:58 +0200

    Update webapp.py

--- python-app/webapp.py
+++ python-app/webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 local = Local()
 
 # MongoDB connection setup
-client = MongoClient('mongodb://root:3yGWpZ7jeS@34.78.116.136:27017/')  # Replace with your MongoDB connection URI
+client = MongoClient('mongodb://root:irKUTs5Dx3@34.38.131.39:27017/')  # Replace with your MongoDB connection URI
 db = client['persi_project']  # Replace with your database name
 collection = db['links']  # Replace with your collection name
 
